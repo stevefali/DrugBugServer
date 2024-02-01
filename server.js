8b16a5a573749b8bf0af738c18410f80fff3ea61
stevefali <stevefaliszewski@gmail.com>
2024-02-01 07:50:15 -0800

    Create temporary endpoint for testing dose notifications - working!

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+const doseNotification = require("./notifications/doseNotifications");
+
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+// Test route for sending notifications. Delete later!!
+app.post("/notify", (req, res) => {
+  const { medicine, name, email } = req.body;
+  try {
+    doseNotification(medicine, name, email);
+    res.status(200).json({ message: `Notification sent to ${email}` });
+  } catch (err) {
+    res.status(500).json({ message: "Error sending test notification!" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
