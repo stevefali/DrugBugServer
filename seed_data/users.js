a2522a181e6d6caf0797c6988706a4e265d7c349
stevefali <stevefaliszewski@gmail.com>
2024-02-01 05:31:20 -0800

    Make and run database seeds

---
+++ seed_data/users.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    id: 1,
+    first_name: "John",
+    last_name: "Guy",
+    email: "someguy@email.com",
+    password: "abc1234",
+  },
+  {
+    id: 2,
+    first_name: "Bilbo",
+    last_name: "Baggins",
+    email: "exampleemail@example.com",
+    password: "password",
+  },
+];
