eff3cd6b839932a88f87a02da29629fcd2767a6b
stevefali <stevefaliszewski@gmail.com>
2024-02-01 08:08:08 -0800

    Add a temporary endpoint for testing refill notifications - working!

--- server.js
+++ server.js
@@ -3,16 +3,27 @@ const app = express();
 require("dotenv").config();
 
 const doseNotification = require("./notifications/doseNotifications");
+const refillNotification = require("./notifications/refillNotifications");
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
 // Test route for sending notifications. Delete later!!
-app.post("/notify", (req, res) => {
+app.post("/notifydose", (req, res) => {
   const { medicine, name, email } = req.body;
   try {
     doseNotification(medicine, name, email);
+    res.status(200).json({ message: `Notification sent to ${email}` });
+  } catch (err) {
+    res.status(500).json({ message: "Error sending test notification!" });
+  }
+});
+
+app.post("/notifyrefill", (req, res) => {
+  const { medicine, name, amount_remaining, amount_unit, email } = req.body;
+  try {
+    refillNotification(medicine, name, amount_remaining, amount_unit, email);
     res.status(200).json({ message: `Notification sent to ${email}` });
   } catch (err) {
     res.status(500).json({ message: "Error sending test notification!" });
