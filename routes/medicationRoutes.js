c67c234cf90aac2a895f114c9ddc618ffd17bc6f
stevefali <stevefaliszewski@gmail.com>
2024-02-02 04:35:46 -0800

    Add post endpoint to add new medication and doses

--- routes/medicationRoutes.js
+++ routes/medicationRoutes.js
@@ -4,4 +4,5 @@ const medicationsController = require("../controllers/medicationsController");
 
 router.route("/:medicationId").put(medicationsController.updateMedication);
 
+router.route("/add").post(medicationsController.addMedication);
 module.exports = router;
