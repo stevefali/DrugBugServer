4c9a0e1aefb47275b217ad7a87494ad3507e7c26
stevefali <stevefaliszewski@gmail.com>
2024-02-04 02:35:56 -0800

    Create login endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -34,4 +34,32 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send("Please fill all required fields.");
+  }
+
+  const user = await knex("users").where({ email: email }).first();
+
+  if (!user) {
+    return res.status(400).send("Invalid email");
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Incorrect Password" });
+  }
+
+  const token = jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_KEY,
+    { expiresIn: "24h" }
+  );
+
+  res.status(200).json({ token: token });
+});
+
 module.exports = router;
