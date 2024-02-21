f9d35b1fa7ccaf08d2e7c17c42ed01ae7576c74c
stevefali <stevefaliszewski@gmail.com>
2024-02-21 13:44:39 -0800

    Log webpush endpoint body

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -100,20 +100,26 @@ router.get("/current", authorize, async (req, res) => {
 
 router.post("/webpush", authorize, async (req, res) => {
   // console.log("webPush called");
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
   try {
-    const { endpoint, keys } = req.body;
-    notificationapi.identifyUser({
-      id: req.verId.toString(),
-      webPushTokens: [
-        {
-          sub: {
-            endpoint: endpoint,
-            keys: keys,
-          },
-        },
-      ],
-    });
+    // const { endpoint, keys } = req.body;
+    // const { PushSubscription } = req.body;
+    // const subAsJson = PushSubscription.toJSON();
+    // console.log(subAsJson);
+
+    console.log(req.body);
+
+    // notificationapi.identifyUser({
+    //   id: req.verId.toString(),
+    //   webPushTokens: [
+    //     {
+    //       sub: {
+    //         endpoint: endpoint,
+    //         keys: keys,
+    //       },
+    //     },
+    //   ],
+    // });
     res.status(200);
   } catch (error) {
     return res
