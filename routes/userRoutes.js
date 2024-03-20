59d5f00c61a2c18ca9ae2410d8269ca60233cbdb
stevefali <stevefaliszewski@gmail.com>
2024-03-19 22:16:03 -0700

    Check webpush endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -120,7 +120,7 @@ router.post("/webpush", async (req, res) => {
     //     },
     //   ],
     // });
-    res.status(200);
+    res.status(200).json({ message: req.body });
   } catch (error) {
     return res
       .status(400)
