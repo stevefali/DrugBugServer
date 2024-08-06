192b7d7dc89eb86174c3868fc503c51c428a2b3c
stevefali <stevefaliszewski@gmail.com>
2024-08-05 23:41:01 -0700

    Start making drug interactions endpoint

--- routes/interactionRoutes.js
+++ routes/interactionRoutes.js
@@ -2,4 +2,6 @@ const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
+const getBoxedWarningEndpoint = require("../utils/networkUtils.js");
+
 module.exports = router;
