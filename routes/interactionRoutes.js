4f56144a357acf1344b29e22522468f552dcda2b
stevefali <stevefaliszewski@gmail.com>
2024-08-07 02:25:25 -0700

    Add network call for each medicine wrapped in anonymous async function to an array so they can be called with Promise.allSettled

--- routes/interactionRoutes.js
+++ routes/interactionRoutes.js
@@ -4,4 +4,31 @@ const router = express.Router();
 
 const getBoxedWarningEndpoint = require("../utils/networkUtils.js");
 
+router.get("/", async (req, res) => {
+  const { interactor } = req.query;
+  const medicines = [
+    "tacrolimus",
+    "amoxicillin",
+    "ozempic",
+    "zanex",
+    "ursodiol",
+  ]; // Hard-coded test medicines for now
+  const fdaCalls = [];
+  for (const medicine of medicines) {
+    fdaCalls.push(async () => {
+      try {
+        return {
+          medicine: medicine,
+          resp: await axios.get(getBoxedWarningEndpoint(medicine)),
+        };
+      } catch (error) {
+        return {
+          medicine: medicine,
+          resp: false,
+        };
+      }
+    });
+  }
+});
+
 module.exports = router;
