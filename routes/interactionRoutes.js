a545a24f156bc441d8e840e938685e343b1d1b5b
stevefali <stevefaliszewski@gmail.com>
2024-08-07 18:53:56 -0700

    Get matches for interactor and push them onto a list to be returned by the endpoint.

--- routes/interactionRoutes.js
+++ routes/interactionRoutes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const getBoxedWarningEndpoint = require("../utils/networkUtils.js");
+const getInteractorMatches = require("../search/interactorSearch.js");
 
 router.get("/", async (req, res) => {
   const { interactor } = req.query;
@@ -33,6 +34,34 @@ router.get("/", async (req, res) => {
   const fdaResponses = await Promise.allSettled(
     fdaCalls.map((fdaCall) => fdaCall())
   );
+
+  const interactionsResponse = [];
+  const noneFound = `No interactions with ${interactor} found.`;
+
+  for (const fdaResponse of fdaResponses) {
+    let medName = fdaResponse.value.medicine;
+    if (fdaResponse.value.resp) {
+      const matches = getInteractorMatches(
+        fdaResponse.value.resp.data.results[0].drug_interactions[0],
+        interactor
+      );
+      interactionsResponse.push({
+        medicine: medName,
+        matches: matches.length > 0 ? matches : noneFound,
+      });
+    } else {
+      interactionsResponse.push({
+        medicine: medName,
+        matches: noneFound,
+      });
+    }
+  }
+
+  res.json({
+    interactionsResponse,
+    disclaimer:
+      "DrugBug's drug interaction search is not meant to replace the advice of a medical professional. Speak to your doctor or pharmacist before making decisions regarding your health and medications.",
+  });
 });
 
 module.exports = router;
